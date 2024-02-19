5b277e3588c8dd92e9d7a5cce8532378b3dcb39f
Sahab <sahabferdous1@gmail.com>
2024-02-19 17:35:24 +0800

    Add prime number getter and checker

---
+++ search/primes.py
@@ -0,0 +1,24 @@
+def get_primes(n):
+    p = 3
+    sieve = [1 for _ in range(n + 1)]
+    while p * p <= n:
+        if sieve[p]:
+            for x in range(p * p, n + 1, 2 * p):
+                sieve[x] = 0
+        p += 2
+    ret = [2]
+    for i in range(3, n + 1, 2):
+        if sieve[i]:
+            ret.append(i)
+    return ret
+
+
+def check_prime(n):
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n < 2:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):  # only odd numbers
+        if n % i == 0:
+            return False
+    return True
